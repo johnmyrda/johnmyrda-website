d1e7790a45822714eae2c07e7b52450d448b9a2c
John Myrda <johnmyrda@gmail.com>
2017-12-17 20:28:48 -0800

    Fix broken links and update libraries

--- projects/scripts.js
+++ projects/scripts.js
@@ -1,18 +1,12 @@
 $("#piboy").backstretch([
-      "https://dl.dropboxusercontent.com/u/124832619/www/projects/piboy-img1.jpg"
-    , "https://dl.dropboxusercontent.com/u/124832619/www/projects/piboy-img2.jpg"
-    , "https://dl.dropboxusercontent.com/u/124832619/www/projects/piboy-img3.jpg"
+      "https://i.imgur.com/MwZjSn8l.jpg"
+    , "https://i.imgur.com/049Y8tYl.jpg"
+    , "https://i.imgur.com/SRKhfqZl.jpg"
   ], {duration: 4000, fade: 1000});
   
 $("#speed-settlers").backstretch([
-      "https://dl.dropboxusercontent.com/u/124832619/www/projects/speed-settlers-img1.jpg"
-    , "https://dl.dropboxusercontent.com/u/124832619/www/projects/speed-settlers-img2.jpg"
-    , "https://dl.dropboxusercontent.com/u/124832619/www/projects/speed-settlers-img3.jpg"
-	, "https://dl.dropboxusercontent.com/u/124832619/www/projects/speed-settlers-img4.jpg"
+      "https://i.imgur.com/PGyuSmdl.jpg"
+    , "https://i.imgur.com/mOCuM1Kl.jpg"
+    , "https://i.imgur.com/l6Pt2fql.jpg"
+	, "https://i.imgur.com/RKGjm8Ml.jpg"
   ], {duration: 3500, fade: 1000});
-  
-$("#treehugger").backstretch([
-	  "https://dl.dropboxusercontent.com/u/124832619/www/projects/treehugger-img1.jpg"
-    , "https://dl.dropboxusercontent.com/u/124832619/www/projects/treehugger-img2.jpg"
-    , "https://dl.dropboxusercontent.com/u/124832619/www/projects/treehugger-img3.png"
-], {duration: 3750, fade: 1000});
